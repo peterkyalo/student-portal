89dd58a50c94e6485a502bbe6504e6e50e0af667
Peter Kyalo <peterkyalo69@gmail>
2022-05-06 12:13:26 +0300

    Third Commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -15,7 +15,7 @@ class StudentController extends Controller
      */
     public function index()
     {
-        $students = Student::all();
+        $students = Student::paginate(3);
         return view('students.index', compact('students'));
     }
 
